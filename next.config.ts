be453cf08ede9c8e8b9fbbba8a2f34540aca6769
Hafiz Arslan Azmat <harslan@nullbrainer.io>
2025-07-13 06:53:59 +0000

    Testing

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -19,5 +20,5 @@ const nextConfig: NextConfig = {
     ],
   },
 };
-
+module.exports = nextConfig
 export default nextConfig;
